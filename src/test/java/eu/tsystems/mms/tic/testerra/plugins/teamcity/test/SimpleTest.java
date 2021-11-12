6a7870886354c8f14818ed5c8b1e1a5b06d20b6d
Martin Großmann <martin.grossmann@t-systems.com>
2021-11-12 09:00:26 +0100

    Added some more tests

--- src/test/java/eu/tsystems/mms/tic/testerra/plugins/teamcity/test/SimpleTest.java
+++ src/test/java/eu/tsystems/mms/tic/testerra/plugins/teamcity/test/SimpleTest.java
@@ -19,12 +19,17 @@ package eu.tsystems.mms.tic.testerra.plugins.teamcity.test;
 
 import eu.tsystems.mms.tic.testframework.annotations.Fails;
 import eu.tsystems.mms.tic.testframework.common.PropertyManager;
+import eu.tsystems.mms.tic.testframework.report.model.context.LogMessage;
+import eu.tsystems.mms.tic.testframework.report.utils.ExecutionContextController;
 import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
 import eu.tsystems.mms.tic.testframework.utils.TimerUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Simple Tests for validating teamcity progess pushers
@@ -36,23 +41,23 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class SimpleTest extends TesterraTest {
 
-    private static final int TEST_DURATION = 2_000;
+    private static final int TEST_DURATION = 1_000;
     private static final boolean FAIL_TESTS = PropertyManager.getBooleanProperty("test.execution.fail.tests", true);
     private static final boolean FAIL_EXPECTED_TESTS = PropertyManager.getBooleanProperty("test.execution.fail.expected.tests", true);
 
-    @Test
+    @Test(priority = 1)
     public void testT01_SimplePassedTest() {
         TimerUtils.sleep(TEST_DURATION);
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(priority = 2)
     public void testT02_SimplePassedTest() {
         TimerUtils.sleep(TEST_DURATION);
         Assert.assertTrue(true);
     }
 
-    @Test
+    @Test(priority = 3)
     public void testT03_SimpleFailedTest() {
         TimerUtils.sleep(TEST_DURATION);
         if (FAIL_TESTS) {
@@ -60,14 +65,14 @@ public class SimpleTest extends TesterraTest {
         }
     }
 
-    @Test(dependsOnMethods = {"testT03_SimpleFailedTest"})
+    @Test(dependsOnMethods = {"testT03_SimpleFailedTest"}, priority = 4)
     public void testT04_SkippedTest() {
         TimerUtils.sleep(TEST_DURATION);
         Assert.assertTrue(true);
     }
 
-    @Test
-    @Fails(ticketId = 1, description = "Failing for reasons")
+    @Test(priority = 5)
+    @Fails(ticketString = "ticket1", description = "Failing for reasons")
     public void testT05_SimpleExpectedFailedTest() {
         TimerUtils.sleep(TEST_DURATION);
         if (FAIL_EXPECTED_TESTS) {
@@ -77,7 +82,7 @@ public class SimpleTest extends TesterraTest {
 
     AtomicInteger counter = new AtomicInteger(0);
 
-    @Test()
+    @Test(priority = 6)
     public void testT06_RetriedTestSecondRunPassed() {
         this.counter.incrementAndGet();
         if (counter.get() == 1) {
@@ -87,6 +92,12 @@ public class SimpleTest extends TesterraTest {
             Assert.assertTrue(true);
         }
 
+    }
+
+    @Test(priority = 999)
+    public void test07_VerifyTeamCityMessages() {
+        Stream<LogMessage> logMessageStream = ExecutionContextController.getCurrentExecutionContext().readMethodContextLessLogs();
+        List<LogMessage> collect = logMessageStream.collect(Collectors.toList());
     }
 
 }
